c5f649cd3dea8461ac21cdb0f813badc71efa3fc
kimkevin <imkimkevin@gmail.com>
2024-12-26 01:39:55 +0900

    todo_table.dart 수정

--- local/lib/models/todo_table.dart
+++ local/lib/models/todo_table.dart
@@ -1,7 +1,7 @@
 import 'package:drift/drift.dart';
 
 class Todos extends Table {
-  IntColumn get id => integer().autoIncrement()(); // Primary Key
+  IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 1, max: 50)();
   BoolColumn get completed => boolean().withDefault(Constant(false))();
 }
