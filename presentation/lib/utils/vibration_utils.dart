4163ac1e56982e8bd3a5c241abc6f5279c9c81de
kimkevin <imkimkevin@gmail.com>
2025-02-12 00:04:25 +0900

    진동 패턴 변경

--- presentation/lib/utils/vibration_utils.dart
+++ presentation/lib/utils/vibration_utils.dart
@@ -2,6 +2,10 @@ import 'package:vibration/vibration.dart';
 
 void triggerStrongVibration() async {
   if (await Vibration.hasVibrator()) {
-    Vibration.vibrate(duration: 200, amplitude: 255);
+    Vibration.vibrate(
+      duration: 200,
+      amplitude: 255,
+      pattern: [0, 100, 100, 100],
+    );
   }
 }
